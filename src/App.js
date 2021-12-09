d6fd403ccda75a89323bf46d9b0a524b2abe3cd0
Juan Carlos <cordovez@el-cordovez.com>
2021-12-09 20:20:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://deliveroo-cordovez.herokuapp.com/"
+    );
+    setData(response.data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(data);
+  return isLoading ? (
+    <p> Data is loading ... </p>
+  ) : (
+    <div>{data.restaurant.name}</div>
   );
 }
 
